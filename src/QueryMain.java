d88349947440a5598b0d36c23ae19181fa011df4
Yunpeng Niu <neilniuyunpeng@gmail.com>
2019-03-09 21:20:20 +0800

    Refactor for main

--- src/QueryMain.java
+++ src/QueryMain.java
@@ -23,135 +23,120 @@ import qp.utils.Tuple;
  * This is main driver program of the query processor
  */
 public class QueryMain {
+    // A writer for output file.
+    private static PrintWriter out;
+    // The number of attributes in the given relation.
+    private static int numOfAttrs;
 
-    static PrintWriter out;
-    static int numAtts;
-
+    /**
+     * The entry point of this RandomDB class.
+     *
+     * @param args are the CLI arguments supplied by the user.
+     */
     public static void main(String[] args) {
-
+        // Premature exit if the number of supplied arguments is wrong.
         if (args.length != 2) {
-            System.out.println("usage: java QueryMain <queryfilename> <resultfile>");
+            System.out.println("usage: java QueryMain <queryFileName> <resultFile>");
             System.exit(1);
         }
 
-
-        /** Enter the number of bytes per page **/
-
+        // Asks user to enter the number of bytes per page.
         System.out.println("enter the number of bytes per page");
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String temp;
         try {
             temp = in.readLine();
-            int pagesize = Integer.parseInt(temp);
-            Batch.setPageSize(pagesize);
+            int pageSize = Integer.parseInt(temp);
+            Batch.setPageSize(pageSize);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-
-        String queryfile = args[0];
-        String resultfile = args[1];
+        // Defines the file name of the input & output.
+        String queryFile = args[0];
+        String resultFile = args[1];
         FileInputStream source = null;
+
+        // Reads the query from the input file.
         try {
-            source = new FileInputStream(queryfile);
+            source = new FileInputStream(queryFile);
         } catch (FileNotFoundException ff) {
-            System.out.println("File not found: " + queryfile);
+            System.out.println("File not found: " + queryFile);
             System.exit(1);
         }
 
-
-        /** scan the query **/
-
+        // Scans the query.
         Scaner sc = new Scaner(source);
         parser p = new parser();
         p.setScanner(sc);
 
-
-        /** parse the query **/
-
+        // Parses the query.
         try {
             p.parse();
         } catch (Exception e) {
-            System.out.println("Exception occured while parsing");
+            System.out.println("Exception occurred while parsing");
             System.exit(1);
         }
 
-        /** SQLQuery is the result of the parsing **/
+        // SQLQuery is the result of the parsing
+        SQLQuery sqlQuery = p.getSQLQuery();
+        int numOfJoin = sqlQuery.getNumJoin();
 
-        SQLQuery sqlquery = p.getSQLQuery();
-        int numJoin = sqlquery.getNumJoin();
-
-
-        /** If there are joins then assigns buffers to each join operator
-         while preparing the plan
-         **/
-        /** As buffer manager is not implemented, just input the number of
-         buffers available
-         **/
-
-
-        if (numJoin != 0) {
+        /*
+         * If there are joins, then assigns buffers to each join operator while preparing
+         * the plan. As buffer manager is not implemented, just input the number of buffers
+         * available.
+         */
+        if (numOfJoin != 0) {
             System.out.println("enter the number of buffers available");
 
             try {
                 temp = in.readLine();
                 int numBuff = Integer.parseInt(temp);
-                BufferManager bm = new BufferManager(numBuff, numJoin);
+                BufferManager bm = new BufferManager(numBuff, numOfJoin);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
 
-
-        /** Let check the number of buffers available is enough or not **/
-
+        // Checks whether the number of buffers available is enough.
         int numBuff = BufferManager.getBuffersPerJoin();
-        if (numJoin > 0 && numBuff < 3) {
-            System.out.println("Minimum 3 buffers are required per a join operator ");
+        if (numOfJoin > 0 && numBuff < 3) {
+            System.out.println("Minimum 3 buffers are required per a join operator.");
             System.exit(1);
         }
 
+        /*
+         * This part is used when some random initial plan is required instead of a
+         * completely optimized plan.
+         */
+        // TODO: uncomment this part after we implemented the randomized algorithm.
+        // RandomInitialPlan rip = new RandomInitialPlan(sqlQuery);
+        // Operator logicalRoot = rip.prepareInitialPlan();
+        // PlanCost pc = new PlanCost();
+        // int initCost = pc.getCost(logicalRoot);
+        // Debug.PPrint(logicalRoot);
+        // System.out.print("   " + initCost);
+        // System.out.println();
 
-/** This part is used When some random initial plan is required instead of comple optimized plan **/
-/**
-
- RandomInitialPlan rip = new RandomInitialPlan(sqlquery);
- Operator logicalroot = rip.prepareInitialPlan();
- PlanCost pc = new PlanCost();
- int initCost = pc.getCost(logicalroot);
- Debug.PPrint(logicalroot);
- System.out.print("   "+initCost);
- System.out.println();
- **/
-
-
-        /** Use random Optimization algorithm to get a random optimized
-         execution plan
-         **/
-
-        RandomOptimizer ro = new RandomOptimizer(sqlquery);
-        Operator logicalroot = ro.getOptimizedPlan();
-        if (logicalroot == null) {
+        // Uses random Optimization algorithm to get a random optimized execution plan.
+        RandomOptimizer randomOptimizer = new RandomOptimizer(sqlQuery);
+        Operator logicalRoot = randomOptimizer.getOptimizedPlan();
+        if (logicalRoot == null) {
             System.out.println("root is null");
             System.exit(1);
         }
 
+        // Prepares the execution plan.
+        Operator root = RandomOptimizer.makeExecPlan(logicalRoot);
 
-        /** preparing the execution plan **/
-
-        Operator root = RandomOptimizer.makeExecPlan(logicalroot);
-
-/** Print final Plan **/
+        // Prints the final execution plan.
         System.out.println("----------------------Execution Plan----------------");
         Debug.PPrint(root);
         System.out.println();
 
-
-/** Ask user whether to continue execution of the program **/
-
-        System.out.println("enter 1 to continue, 0 to abort ");
-
-
+        // Asks user whether to continue execution of the program.
+        System.out.println("enter 1 to continue, 0 to abort");
         try {
             temp = in.readLine();
             int flag = Integer.parseInt(temp);
@@ -163,48 +148,68 @@ public class QueryMain {
             e.printStackTrace();
         }
 
-        long starttime = System.currentTimeMillis();
+        // Records down the start time of the query execution.
+        long startTime = System.currentTimeMillis();
 
-
-        if (root.open() == false) {
-            System.out.println("Root: Error in opening of root");
+        // Tries to open the logical root.
+        if (!root.open()) {
+            System.out.println("Root: error in opening of root");
             System.exit(1);
         }
+
+        // Creates a writer for output file.
         try {
-            out = new PrintWriter(new BufferedWriter(new FileWriter(resultfile)));
+            out = new PrintWriter(new BufferedWriter(new FileWriter(resultFile)));
         } catch (IOException io) {
-            System.out.println("QueryMain:error in opening result file: " + resultfile);
+            System.out.println("QueryMain: error in opening result file: " + resultFile);
             System.exit(1);
         }
 
-
-        /** print the schema of the result **/
+        // Prints the schema of the result.
         Schema schema = root.getSchema();
-        numAtts = schema.getNumCols();
+        numOfAttrs = schema.getNumCols();
         printSchema(schema);
-        Batch resultbatch;
 
-
-        /** print each tuple in the result **/
-
-
-        while ((resultbatch = root.next()) != null) {
-            for (int i = 0; i < resultbatch.size(); i++) {
-                printTuple(resultbatch.elementAt(i));
+        // Prints each tuple in the result.
+        Batch resultBatch = root.next();
+        while (resultBatch != null) {
+            for (int i = 0; i < resultBatch.size(); i++) {
+                printTuple(resultBatch.elementAt(i));
             }
+            resultBatch = root.next();
         }
+
+        // Closes the resources gracefully.
         root.close();
         out.close();
 
-        long endtime = System.currentTimeMillis();
-        double executiontime = (endtime - starttime) / 1000.0;
-        System.out.println("Execution time = " + executiontime);
+        // Records down the end time of the query execution and thus calculates the execution time.
+        long endTime = System.currentTimeMillis();
+        double executionTime = (endTime - startTime) / 1000.0;
+        System.out.println("Execution time = " + executionTime);
 
     }
 
+    /**
+     * Prints out a given schema.
+     *
+     * @param schema is the schema to be printed.
+     */
+    private static void printSchema(Schema schema) {
+        for (int i = 0; i < numOfAttrs; i++) {
+            Attribute attr = schema.getAttribute(i);
+            out.print(attr.getTabName() + "." + attr.getColName() + "  ");
+        }
+        out.println();
+    }
 
-    protected static void printTuple(Tuple t) {
-        for (int i = 0; i < numAtts; i++) {
+    /**
+     * Prints out a given tuple.
+     *
+     * @param t is the tuple to be printed.
+     */
+    private static void printTuple(Tuple t) {
+        for (int i = 0; i < numOfAttrs; i++) {
             Object data = t.dataAt(i);
             if (data instanceof Integer) {
                 out.print(((Integer) data).intValue() + "\t");
@@ -216,20 +221,4 @@ public class QueryMain {
         }
         out.println();
     }
-
-    protected static void printSchema(Schema schema) {
-        for (int i = 0; i < numAtts; i++) {
-            Attribute attr = schema.getAttribute(i);
-            out.print(attr.getTabName() + "." + attr.getColName() + "  ");
-        }
-        out.println();
-    }
-
 }
-
-
-
-
-
-
-
