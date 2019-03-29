512c5939e1dc99d6b35b37466d7bf45ea29bd56a
Yunpeng Niu <neilniuyunpeng@gmail.com>
2019-03-30 00:17:43 +0800

    Implement I/O

--- src/qp/operators/Sort.java
+++ src/qp/operators/Sort.java
@@ -1,5 +1,7 @@
 package qp.operators;
 
+import java.io.EOFException;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -8,7 +10,6 @@ import java.util.Vector;
 
 import qp.utils.Attribute;
 import qp.utils.Batch;
-import qp.utils.Schema;
 import qp.utils.Tuple;
 
 /**
@@ -53,7 +54,7 @@ public class Sort extends Operator {
         // Phase 1: generate sorted runs using in-memory sorting algorithms.
         int numOfRuns = generateSortedRuns();
         // Phase 2: merge sorted runs together (could be in multiple passes).
-        return mergeRuns(numOfRuns) == 1;
+        return mergeRuns(numOfRuns, 1) == 1;
     }
 
     /**
@@ -80,12 +81,14 @@ public class Sort extends Operator {
             // Sorts the tuples using in-memory sorting algorithms.
             tuplesInRun.sort(this::compareTuples);
 
-            // Stores the sorted result into disk.
-            String fileName = getSortedRunFileName(numOfRuns);
+            // Stores the sorted result into disk (phase 1 is the 0th pass).
+            String fileName = getSortedRunFileName(0, numOfRuns);
             try {
                 ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(fileName));
                 // TODO: do we need to write each batch to a single file?
-                stream.writeObject(tuplesInRun);
+                for (Tuple tuple: tuplesInRun) {
+                    stream.writeObject(tuple);
+                }
                 stream.close();
             } catch (IOException e) {
                 System.err.println("Sort: unable to write sortedRun with ID=" + numOfRuns + " due to " + e);
@@ -105,9 +108,10 @@ public class Sort extends Operator {
      * buffers to minimize the number of passes.
      *
      * @param numOfRuns is the number of sorted runs to be merged.
+     * @param passID is the ID of the current pass.
      * @return the number of sorted runs after one round of merge.
      */
-    private int mergeRuns(int numOfRuns) {
+    private int mergeRuns(int numOfRuns, int passID) {
         // Exits if there is no more than 1 run (which means there is no need to merge anymore).
         if (numOfRuns <= 1) {
             return numOfRuns;
@@ -117,12 +121,20 @@ public class Sort extends Operator {
         int numOfOutputRuns = 0;
         for (int startRunID = 0; startRunID < numOfRuns; startRunID = startRunID + numOfBuffers - 1) {
             int endRunID = Math.min(startRunID + numOfBuffers - 1, numOfRuns);
-            mergeRunsBetween(startRunID, endRunID);
+            try {
+                mergeRunsBetween(startRunID, endRunID, passID, numOfOutputRuns);
+            } catch (IOException e) {
+                System.err.printf("Sort: cannot mergeRuns on passID=%d for [%d, %d) due to %s", passID, startRunID, endRunID, e);
+                System.exit(1);
+            } catch (ClassNotFoundException e) {
+                System.err.printf("Sort: class not found on passID=%d for [%d, %d) due to %s", passID, startRunID, endRunID, e);
+                System.exit(1);
+            }
             numOfOutputRuns++;
         }
 
         // Continues to the next round using a recursive call.
-        return mergeRuns(numOfOutputRuns);
+        return mergeRuns(numOfOutputRuns, passID + 1);
     }
 
     /**
@@ -131,21 +143,60 @@ public class Sort extends Operator {
      *
      * @param startRunID is the sorted run ID of the lower bound (inclusive).
      * @param endRunID is the sorted run ID of the upper bound (exclusive).
+     * @param passID is the ID of the current pass.
+     * @param outID is the sorted run ID of the output.
      *
      * @implNote we effectively implement a k-way merge sort here.
      */
-    private void mergeRunsBetween(int startRunID, int endRunID) {
+    private void mergeRunsBetween(int startRunID, int endRunID, int passID, int outID) throws IOException, ClassNotFoundException {
         // Each input sorted run has 1 buffer page.
         Batch[] inBatches = new Batch[endRunID - startRunID];
+        // Records whether we have reached end-of-stream for a certain input run.
+        boolean[] inEos = new boolean[endRunID - startRunID];
         // Each input sorted run has one stream to read from.
         ObjectInputStream[] inStreams = new ObjectInputStream[endRunID - startRunID];
+        for (int i = startRunID; i < endRunID; i++) {
+            String inputFileName = getSortedRunFileName(passID, i);
+            ObjectInputStream inStream = new ObjectInputStream(new FileInputStream(inputFileName));
+            inStreams[i - startRunID] = inStream;
+
+            // Fills in the data from an input run.
+            try {
+                inBatches[i - startRunID] = readInputBatch(inStream);
+            } catch (EOFException eof) {
+                inEos[i - startRunID] = true;
+                inBatches[i - startRunID] = null;
+            }
+        }
+
         // Only 1 buffer page for output.
         Batch outBatch = new Batch(batchSize);
+        // The stream for output buffer.
+        String outputFileName = getSortedRunFileName(passID + 1, outID);
+        ObjectOutputStream outStream = new ObjectOutputStream(new FileOutputStream(outputFileName));
 
-        // Establishes the streams and reads the first pages of each input sorted run.
-        for (int i = 0; i < endRunID - startRunID; i++) {
-
+        // Closes the resources used.
+        for (ObjectInputStream inStream: inStreams) {
+            inStream.close();
         }
+        outStream.close();
+    }
+
+    /**
+     * Reads in a batch of tuples to simulate reading a page from disk.
+     *
+     * @param stream is the input stream to read from.
+     * @return an {@link Batch} read from the input stream.
+     * @throws IOException for any of the usual I/O related exceptions.
+     * @throws ClassNotFoundException when the input content is serialized as a class that cannot be found.
+     */
+    private Batch readInputBatch(ObjectInputStream stream) throws IOException, ClassNotFoundException {
+        Batch inBatch = new Batch(batchSize);
+        while (!inBatch.isFull()) {
+            Tuple data = (Tuple) stream.readObject();
+            inBatch.add(data);
+        }
+        return inBatch;
     }
 
     /**
@@ -174,9 +225,10 @@ public class Sort extends Operator {
     /**
      * Provides the file name of a generated sorted run based on its run ID.
      *
+     * @param passID is the ID of the current pass.
      * @param runID is the ID of the sorted run.
      */
-    private String getSortedRunFileName(int runID) {
-        return "Sort-run-" + runID;
+    private String getSortedRunFileName(int passID, int runID) {
+        return "Sort-run-" + passID + "-" + runID;
     }
 }
