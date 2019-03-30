ff3ac7896513919a53fecfb16c9f7853cb2f757a
dalessr <dale.sunshengran@gmail.com>
2019-03-30 18:06:24 +0800

    temp

--- src/qp/operators/SortMergeJoin.java
+++ src/qp/operators/SortMergeJoin.java
@@ -2,8 +2,19 @@ package qp.operators;
 
 import qp.utils.Attribute;
 import qp.utils.Batch;
+import qp.utils.Tuple;
 
 public class SortMergeJoin extends Join {
+    // The number of tuples per output batch.
+    private int batchSize;
+
+    // The buffer for the left input stream.
+    private Batch leftBatch;
+    // The buffer for the right input stream.
+    private Batch rightBatch;
+    // The output buffer.
+    private Batch outBatch;
+
     // Index of the join attribute in left table
     private int leftIndex;
     // Index of the join attribute in right table
@@ -40,9 +51,12 @@ public class SortMergeJoin extends Join {
      */
     @Override
     public boolean open() {
-        // Sorts the left & right relation.
+        // Sorts the left relation.
         left.open();
-        right.open();
+
+        // Selects the number of tuples per page based tuple size.
+        int tupleSize = schema.getTupleSize();
+        batchSize = Batch.getPageSize() / tupleSize;
 
         // Gets the join attribute from left & right table.
         Attribute leftAttr = con.getLeft();
@@ -54,7 +68,7 @@ public class SortMergeJoin extends Join {
         leftCursor = 0;
         rightCursor = 0;
         eosLeft = false;
-        eosRight = false;
+        eosRight = true;
 
         return super.open();
     }
@@ -66,18 +80,79 @@ public class SortMergeJoin extends Join {
      */
     @Override
     public Batch next() {
-        return null;
+        // Returns empty if the left table reaches end-of-stream.
+        if (eosLeft) {
+            close();
+            return null;
+        }
+
+        outBatch = new Batch(batchSize);
+        while (!outBatch.isFull()) {
+            // Checks whether we need to read a new page from the left table.
+            if (leftCursor == 0 && eosRight) {
+                // Fetches a new page from left table.
+                leftBatch = left.next();
+                if (leftBatch == null) {
+                    eosLeft = true;
+                    return outBatch;
+                }
+                right.open();
+                eosRight = false;
+            }
+
+            // Continuously probe the right table until we hit the end-of-stream.
+            while (!eosRight) {
+                if (leftCursor == 0 && rightCursor == 0) {
+                    rightBatch = right.next();
+                    if (rightBatch == null) {
+                        eosRight = true;
+                        break;
+                    }
+                }
+
+                for (int i = leftCursor; i < leftBatch.size(); i++) {
+                    for (int j = rightCursor; j < rightBatch.size(); j++) {
+                        Tuple leftTuple = leftBatch.elementAt(i);
+                        Tuple rightTuple = rightBatch.elementAt(j);
+
+                        // Adds the tuple if satisfying the join condition.
+                        if (leftTuple.checkJoin(rightTuple, leftIndex, rightIndex)) {
+                            Tuple outTuple = leftTuple.joinWith(rightTuple);
+                            outBatch.add(outTuple);
+
+                            // Checks whether the output buffer is full.
+                            if (outBatch.isFull()) {
+                                if (i == leftBatch.size() - 1 && j == rightBatch.size() - 1) {
+                                    leftCursor = 0;
+                                    rightCursor = 0;
+                                } else if (i != leftBatch.size() - 1 && j == rightBatch.size() - 1) {
+                                    leftCursor = i + 1;
+                                    rightCursor = 0;
+                                } else {
+                                    leftCursor = i;
+                                    rightCursor = j + 1;
+                                }
+
+                                // Returns since we have already produced a complete page of matching tuples.
+                                return outBatch;
+                            }
+                        }
+                    }
+                    rightCursor = 0;
+                }
+                leftCursor = 0;
+            }
+        }
+        return outBatch;
     }
 
     /**
-     * Closes this operator by closing left & right operators.
+     * Closes this operator.
      *
-     * @return true if both left and right operators are closed successfully.
+     * @return true if the operator is closed successfully.
      */
     @Override
     public boolean close() {
-        boolean isLeftClosed = left.close();
-        boolean isRightClosed = right.close();
-        return isLeftClosed & isRightClosed;
+        return true;
     }
 }
