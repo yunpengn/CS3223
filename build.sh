7368f989682d8617f500bbc982a5747c072a8837
Yunpeng Niu <neilniuyunpeng@gmail.com>
2019-03-30 15:38:12 +0800

    Add dependencies

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ javac -d ${COMPONENT}/classes ${COMPONENT}/src/qp/utils/*.java
 javac -d ${COMPONENT}/classes ${COMPONENT}/src/qp/parser/*.java
 javac -d ${COMPONENT}/classes ${COMPONENT}/src/qp/operators/*.java
 javac -d ${COMPONENT}/classes ${COMPONENT}/src/qp/optimizer/*.java
-javac -d ${COMPONENT}/classes ${COMPONENT}/test/*.java
+javac -d ${COMPONENT}/classes ${COMPONENT}/testcases/*.java
 javac -d ${COMPONENT}/classes ${COMPONENT}/src/QueryMain.java
 
 echo "Project has been built successfully!"
