de052e13a5cda72a6b443ee04d67aaf4955fd7fa
Yunpeng Niu <neilniuyunpeng@gmail.com>
2019-03-30 16:45:36 +0800

    Add more tests

--- test/qp/parser/ParserTest.java
+++ test/qp/parser/ParserTest.java
@@ -8,6 +8,7 @@ import java.util.Vector;
 import org.junit.Test;
 
 import qp.utils.Attribute;
+import qp.utils.Condition;
 import qp.utils.SQLQuery;
 
 /**
@@ -48,12 +49,44 @@ public class ParserTest {
         assertEquals(0, query.getNumJoin());
     }
 
+    @Test
+    public void selectWithWhereEqual() throws Exception {
+        SQLQuery query = parseString("SELECT customers.age FROM customers WHERE customers.size = \"100\"");
+        assertEquals(1, query.getFromList().size());
+        assertEquals("customers", query.getFromList().elementAt(0));
+        assertEquals(1, query.getSelectionList().size());
+        assertSelectCondition(query.getSelectionList(), 0, "size", "100", Condition.EQUAL);
+        assertEquals(1, query.getProjectList().size());
+        assertAttributeName(query.getProjectList(), 0, "age");
+        assertEquals(0, query.getNumJoin());
+    }
+
+    @Test
+    public void selectWithWhereGreaterThan() throws Exception {
+        SQLQuery query = parseString("SELECT customers.name FROM customers WHERE customers.age > \"25\"");
+        assertEquals(1, query.getFromList().size());
+        assertEquals("customers", query.getFromList().elementAt(0));
+        assertEquals(1, query.getSelectionList().size());
+        assertSelectCondition(query.getSelectionList(), 0, "age", "25", Condition.GREATER_THAN);
+        assertEquals(1, query.getProjectList().size());
+        assertAttributeName(query.getProjectList(), 0, "name");
+        assertEquals(0, query.getNumJoin());
+    }
+
     private SQLQuery parseString(String input) throws Exception {
         StringReader reader = new StringReader(input);
         Scanner scanner = new Scanner(reader);
         parser p = new parser(scanner);
         p.parse();
         return p.getSQLQuery();
+    }
+
+    private void assertSelectCondition(Vector selectionList, int id, String leftAttrName, String rightValue, int operator) {
+        Condition condition = (Condition) selectionList.elementAt(id);
+        assertEquals(Condition.SELECT, condition.getCondType());
+        assertEquals(leftAttrName, condition.getLeft().getColName());
+        assertEquals(rightValue, condition.getRight());
+        assertEquals(operator, condition.getOperator());
     }
 
     private void assertAttributeName(Vector projectList, int id, String expectedName) {
