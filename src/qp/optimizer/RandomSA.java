628c4c568380c1f97f0e79a441b3507c4bb62c50
Yunpeng Niu <neilniuyunpeng@gmail.com>
2019-04-01 23:42:24 +0800

    Implement SA

--- src/qp/optimizer/RandomSA.java
+++ src/qp/optimizer/RandomSA.java
@@ -1,12 +1,16 @@
 package qp.optimizer;
 
 import qp.operators.Operator;
+import qp.utils.RandomNum;
 import qp.utils.SQLQuery;
 
 /**
  * Defines a randomized query optimizer using the Simulated Annealing (SA) algorithm.
  */
 public class RandomSA extends RandomOptimizer {
+    private static final double END_TEMPERATURE = 1;
+    private static final double ALPHA = 0.8;
+
     /**
      * Constructor of RandomOptimizer.
      *
@@ -30,7 +34,7 @@ public class RandomSA extends RandomOptimizer {
         // The current & final plan.
         Operator minPlan = rip.prepareInitialPlan();
         int minCost = Integer.MAX_VALUE;
-        int temperature = numOfJoin * 5;
+        double temperature = numOfJoin * 2;
 
         // Premature exits if there is no join in the query.
         if (numOfJoin == 0) {
@@ -38,10 +42,35 @@ public class RandomSA extends RandomOptimizer {
             return minPlan;
         }
 
-        while (temperature > 0) {
-            temperature--;
+        // Continues until the temperature has dropped below a certain threshold (i.e., frozen).
+        while (temperature > END_TEMPERATURE) {
+            Operator initPlanCopy = (Operator) minPlan.clone();
+            Operator currentPlan = getNeighbor(initPlanCopy);
+            int currentCost = printPlanCostInfo("Neighbor", currentPlan);
+
+            if (currentCost < minCost || judge(temperature, currentCost, minCost)) {
+                minPlan = currentPlan;
+                minCost = currentCost;
+            }
+
+            // Reduces the temperature.
+            temperature *= ALPHA;
         }
 
-        return null;
+        return minPlan;
+    }
+
+    /**
+     * Judges whether we accept this uphill move according to the annealing probability function.
+     *
+     * @param temperature is the current annealing temperature.
+     * @param value1 is the first value.
+     * @param value2 is the second value.
+     * @return true if we should accept this move.
+     */
+    private boolean judge(double temperature, int value1, int value2) {
+        int delta = Math.abs(value1 - value2);
+        double prob = Math.exp(-delta / temperature);
+        return RandomNum.randDouble() < prob;
     }
 }
