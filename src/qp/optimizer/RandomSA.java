2b4d6610da7d5239337ae2dfd4a4d3cf9e51c347
Yunpeng Niu <neilniuyunpeng@gmail.com>
2019-04-01 23:58:40 +0800

    Update

--- src/qp/optimizer/RandomSA.java
+++ src/qp/optimizer/RandomSA.java
@@ -9,7 +9,7 @@ import qp.utils.SQLQuery;
  */
 public class RandomSA extends RandomOptimizer {
     private static final double END_TEMPERATURE = 1;
-    private static final double ALPHA = 0.8;
+    private static final double ALPHA = 0.85;
 
     /**
      * Constructor of RandomOptimizer.
@@ -33,8 +33,8 @@ public class RandomSA extends RandomOptimizer {
 
         // The current & final plan.
         Operator minPlan = rip.prepareInitialPlan();
+        Transformations.modifySchema(minPlan);
         int minCost = Integer.MAX_VALUE;
-        double temperature = numOfJoin * 2;
 
         // Premature exits if there is no join in the query.
         if (numOfJoin == 0) {
@@ -43,20 +43,31 @@ public class RandomSA extends RandomOptimizer {
         }
 
         // Continues until the temperature has dropped below a certain threshold (i.e., frozen).
-        while (temperature > END_TEMPERATURE) {
-            Operator initPlanCopy = (Operator) minPlan.clone();
-            Operator currentPlan = getNeighbor(initPlanCopy);
-            int currentCost = printPlanCostInfo("Neighbor", currentPlan);
+        for (double temperature = numOfJoin * 2; temperature > END_TEMPERATURE; temperature *= ALPHA) {
+            Operator initPlan = rip.prepareInitialPlan();
+            Transformations.modifySchema(minPlan);
+            int initCost = printPlanCostInfo("Initial Plan", initPlan);
 
-            if (currentCost <= minCost || judge(temperature, currentCost, minCost)) {
-                minPlan = currentPlan;
-                minCost = currentCost;
+            // Continues until we reach equilibrium.
+            for (int i = 0; i < 8 * numOfJoin; i++) {
+                Operator currentPlan = getNeighbor(initPlan);
+                int currentCost = printPlanCostInfo("Neighbor", currentPlan);
+
+                if (currentCost <= initCost || judge(temperature, currentCost, initCost)) {
+                    initPlan = currentPlan;
+                    initCost = currentCost;
+                }
             }
 
-            // Reduces the temperature.
-            temperature *= ALPHA;
+            // Tries to update the global optimal solution if necessary.
+            printPlanCostInfo("Local Minimum", initPlan, initCost);
+            if (initCost < minCost) {
+                minPlan = initPlan;
+                minCost = initCost;
+            }
         }
 
+        printPlanCostInfo("Final Plan", minPlan, minCost);
         return minPlan;
     }
 
